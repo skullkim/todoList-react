61cd960d3693cea3448ce98769b6694bc1d4d362
skullKim <space1878@naver.com>
2021-07-03 16:22:16 +0900

    추가: react files

---
+++ todo/src/InputTodoComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const inputTodo = styled.input`
+    color: lightgray;
+    background-color: black;
+`;
+
+const addTodo = styled`
+    background-color: gray;
+    color: white;
+`;
+
+const
