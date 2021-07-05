c478c43950dc73edeac5cbfb89d6a354eb237c00
skullKim <space1878@naver.com>
2021-07-05 20:36:55 +0900

    추가: to do완료기 완료 체크 표시 기능 완료

--- todo/src/App.js
+++ todo/src/App.js
@@ -9,23 +9,33 @@ import styles from "./styles/App-style.module.scss";
 function App() {
     const [toDoList, setToDoList] = useState([]);
     const [toDoId, setToDoId] = useState(0);
+    const [checked, setChecked] = useState(false);
     const addToDo = (toDo) => {
         const newToDo = toDoList.concat({
             id: toDoId,
             toDo,
+            checked,
         });
         setToDoList(newToDo);
         setToDoId(toDoId + 1);
+        setChecked(false);
     }
     const deleteToDo = (key) => {
         const newToDo = toDoList.filter((toDo) => toDo.id !== key);
+        setToDoList(newToDo);
+    }
+    const checkToDo = (key) => {
+        console.log(key);
+        const newToDo = toDoList.map((toDo) => {
+            return key === toDo.id ? ({...toDo, checked: !toDo.checked}) : toDo;
+        });
         setToDoList(newToDo);
     }
     return (
      <div className={styles.div}>
         <HeaderComponent/>
         <InputTodoComponent addToDo={addToDo}/>
-        <ToDoListComponent toDoList={toDoList} deleteToDo={deleteToDo}/>
+        <ToDoListComponent toDoList={toDoList} deleteToDo={deleteToDo} checkToDo={checkToDo}/>
      </div>
   );
 }
--- todo/src/TodoListComponent.js
+++ todo/src/TodoListComponent.js
@@ -1,7 +1,8 @@
 import React, {useState, useEffect} from 'react';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faCalendarTimes} from '@fortawesome/free-solid-svg-icons';
+import {MdCheckBoxOutlineBlank, MdCheckBox} from 'react-icons/md'
 
 const ListBox = styled.div `
     width: 500px;
@@ -10,13 +11,24 @@ const ListBox = styled.div `
     overflow: scroll;
 `;
 
+const List = styled.span `
+    ${(props) =>
+        props.checked && 
+        css `
+            color: lightgray;
+            text-decoration: line-through;
+        `    
+    }
+`;
+
 const ToDoBox = styled.div`
     width: 500px;
     height: 50px;
     border: lightgray solid 2px;
+    display: flex;
 `;
 
-const ToDoListComponent = ({toDoList, deleteToDo}) => {
+const ToDoListComponent = ({toDoList, deleteToDo, checkToDo}) => {
     const [currToDo, setCurrTodo] = useState();
     const handleClick = (key) => {
         deleteToDo(key);
@@ -24,7 +36,11 @@ const ToDoListComponent = ({toDoList, deleteToDo}) => {
     useEffect(() => {
         const toDo = toDoList.map((schedule) => {
             return (<ToDoBox key={schedule.id}>
-                        {schedule.toDo}
+                {schedule.checked ?
+                    <MdCheckBox onClick={() => checkToDo(schedule.id)}/> :
+                    <MdCheckBoxOutlineBlank onClick={() => checkToDo(schedule.id)}/>
+                }
+                        <List checked={schedule.checked}>{schedule.toDo}</List>
                         <FontAwesomeIcon icon={faCalendarTimes} onClick={(e) => {handleClick(schedule.id)}}/>
                     </ToDoBox>);
         });
